c611cd323bcb6d71f46608f8447ac19d8be86dc9
nyantama <r_nyantama0616@yahoo.co.jp>
2023-11-15 23:35:25 +0900

    TrainRecordのテストデータをseeds.rbで投入するようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+NOTES = [50, 52, 53, 55, 57, 59, 62, 64, 65, 67, 69, 71]
+interval_records = []
+100.times do |i|
+    record = {
+        note0: 60,
+        note1: NOTES.sample,
+        missCount: (0..3).to_a.sample,
+        duration: 350.step(1000, 10).to_a.sample
+    }
+    interval_records.push(record)
+end
+
+TrainRecord.create(json: JSON.dump(interval_records))
