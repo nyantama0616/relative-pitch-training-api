6cf10addf4b9aa8f5f3d807908caea60517da5f0
nyantama <r_nyantama0616@yahoo.co.jp>
2024-01-08 13:57:24 +0900

    アンケート内容を更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: { message: "Email has already been taken" }
     has_many :train_records, dependent: :destroy
+    has_many :questionnaire, dependent: :destroy
 
     def info
         json = as_json(only: [:id, :user_name, :email, :image_path])
